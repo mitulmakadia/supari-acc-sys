a9a4a99eccd206c3451086dd1b82c594bd0edbb3
Harshmakadia <harsh.makadia@marutitech.com>
2015-08-26 14:04:16 +0530

    gittest

--- laravel/resources/views/createuser.blade.php
+++ laravel/resources/views/createuser.blade.php
@@ -8,6 +8,6 @@ $Email=$_POST['Email'];
 
 //Insert query
 $query = mysql_query("insert into userdetail(FirstName, LastName, Email) values ('$FirstName', '$LastName', '$Email')");
-echo "Form Submitted Succesfully";
+echo "Form Submitted Succesfully
 mysql_close($connection); // Connection Closed
 ?>
